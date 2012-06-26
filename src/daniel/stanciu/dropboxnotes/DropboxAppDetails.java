19590721fd1675c6340812931d804f9ab8349f16
dstanciu <dstanciu@gmail.com>
2012-06-26 06:36:46 +0000

    initial commit

---
+++ src/daniel/stanciu/dropboxnotes/DropboxAppDetails.java
@@ -0,0 +1,12 @@
+package daniel.stanciu.dropboxnotes;
+
+import com.dropbox.client2.session.Session.AccessType;
+
+public class DropboxAppDetails {
+
+	// for Dropbox integration
+	final static String APP_KEY = "CHANGE_ME";
+	final static String APP_SECRET = "CHANGE_ME";
+	final static AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
+
+}
